7b2654d80cfe4836afe25b0c38bc11ee8d757e02
Dylan <dylan.lewis@phs.scot>
2026-03-17 18:41:04 +0000

    example outputs

--- examples/bslib_html_report.R
+++ examples/bslib_html_report.R
@@ -4,6 +4,15 @@ library(dplyr)
 library(ggiraph)
 library(bslib)
 
+# data
+year <- 2008
+location <- "Biscoe"
+
+data <- penguins |>
+  filter(year == year,
+         island == location) |>
+  select(species, sex, bill_len:body_mass)
+
 # table
 my_table <- function(data){
   datatable(data,
@@ -61,7 +70,7 @@ overview <- nav_panel(
   layout_columns(
     col_widths = breakpoints(xs = c(-2,8,-2), xxl = c(-3,6,-3)),
     "This page shows all data on penguins",
-    my_table(penguins)
+    my_table(data)
   )
 )
 
@@ -70,16 +79,60 @@ flipper_length <- nav_panel(
   layout_columns(
     col_widths = breakpoints(xs = c(-2,8,-2), xxl = c(-3,6,-3)),
     "This page examines the flipper length of penguins",
-    plots_by_sex(penguins)
+    plots_by_sex(data)
   )
 )
 
 # report
-page_navbar(
+
+output <- page_navbar(
   title = "Penguin report",
   fillable = FALSE,
   navbar_options = navbar_options(
     bg = "#3F3685"
   ),
+  overview,
   flipper_length
 )
+
+### the following code is experimental:
+
+save_self_contained_html <- function(taglist, output_filepath) {
+  # Normalize output path
+  output_filepath <- xfun::normalize_path(output_filepath)
+  
+  # Create a temporary directory for intermediate files
+  temp_dir <- tempfile()
+  dir.create(temp_dir)
+  on.exit(unlink(temp_dir, recursive = TRUE), add = TRUE)
+  
+  # Temporary HTML file inside temp_dir
+  temp_html <- file.path(temp_dir, "temp.html")
+  
+  # Save the taglist as HTML (with /libs folder if needed)
+  htmltools::save_html(taglist, file = temp_html, libdir = file.path(temp_dir, "libs"))
+  
+  # Remove <!DOCTYPE> lines for pandoc
+  input_lines <- readLines(temp_html, warn = FALSE)
+  writeLines(input_lines[!grepl("<!DOCTYPE", input_lines, fixed = TRUE)], temp_html)
+  
+  # Create a minimal template for pandoc
+  template <- tempfile(fileext = ".html")
+  on.exit(unlink(template), add = TRUE)
+  xfun::write_utf8("$body$", template)
+  
+  # Determine pandoc format
+  from <- if (rmarkdown::pandoc_available("1.17")) "markdown_strict" else "markdown"
+  
+  # Convert to self-contained HTML
+  rmarkdown::pandoc_convert(
+    input = temp_html,
+    from = from,
+    output = output_filepath,
+    options = c("--embed-resources", "--standalone", "--template", template)
+  )
+  
+  invisible(output_filepath)
+}
+
+# save_self_contained_html(output, "examples/bslib_html_report.html")
